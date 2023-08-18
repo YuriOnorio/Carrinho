123b22e56e00c378561606bca51d89e3c0128656
YuriOnorio <yronorio@gmail.com>
2023-08-18 12:23:45 -0300

    Colocando o CSS

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { StyleSheet, Image, Dimensions, Text, View } from "react-native";
 import topo from "../../../assets/topo.png";
+import logo from "../../../assets/logo.png";
 
 const width = Dimensions.get("screen").width;
+
 export default function Carrinho() {
-  return (
-    <>
+  return ( <>
       <Image source={topo} style={estilos.topo} />
-      <Text style={estilos.titulo}>Detelhe do Carrinho AA</Text>
-      <View>
-        <Text>Carrinho de compras</Text>
-        <Text>Fazenda Etec Cidade do Livro</Text>
-        <Text>
-          Uma cesta com produtos selecionados cuidadosamente da fazenda direto
-          para sua cozinha
+      <Text style={estilos.titulo}>Detelhe do Carrinho</Text>
+
+      <View style={estilos.carrinho}>
+
+        <Text style={estilos.titulocarrinho}>Carrinho de compras</Text>
+
+        <View style={estilos.fazenda}>
+          <Image style={estilos.imagemFazenda} source={logo}/>
+          <Text style={estilos.nomefazenda}>Fazenda Etec Cidade do Livro</Text>
+        </View>
+        
+        <Text style={estilos.descricao}>Uma cesta com produtos selecionados 
+        cuidadosamente da fazenda direto para a sua cozinha
         </Text>
-        <Text>R$ 40,00</Text>
+
+        <Text style={estilos.preco}>R$ 40,00</Text>
+        
       </View>
     </>
   );
@@ -37,4 +46,45 @@ const estilos = StyleSheet.create({
     position: "absolute",
     padding: 16,
   },
+  
+  titulocarrinho: {
+    fontSize: 26,
+    lineHeight: 42,
+    color: "#464646",
+    fontFamily: "MontserratBold"
+  },
+
+  carrinho: {
+    paddingVertical: 8,
+    paddingHorizontal: 16
+  },
+
+  nomefazenda: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12,
+    fontFamily: "MontserratBold"
+  },
+
+  imagemFazenda: {
+    width: 32,
+    height: 32
+  },
+
+  fazenda: {
+    flexDirection: "row",
+    paddingVertical: 12
+  },
+  
+  descricao: {
+    color: "#a3a3a3",
+    fontStyle: "italic"
+  },
+
+  preco: {
+    fontSize: 26,
+    lineHeight: 42,
+    color: "#2a9f85",
+    marginTop: 8
+  }
 });
