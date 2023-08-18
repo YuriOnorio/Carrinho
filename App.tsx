300bd9cf285c777d3340d2c85c82aec62b45bc85
YuriOnorio <yronorio@gmail.com>
2023-08-18 11:25:26 -0300

    Criação

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { View, StatusBar, SafeAreaView } from "react-native";
+import Carrinho from "./src/pages/Carrinho";
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Carrinho />
+    </SafeAreaView>
+  );
+}
---
+++ src/pages/Carrinho/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { StyleSheet, Image, Dimensions, Text, View } from "react-native";
+import topo from "../../../assets/topo.png";
+
+const width = Dimensions.get("screen").width;
+export default function Carrinho() {
+  return (
+    <>
+      <Image source={topo} style={estilos.topo} />
+      <Text style={estilos.titulo}>Detelhe do Carrinho AA</Text>
+      <View>
+        <Text>Carrinho de compras</Text>
+        <Text>Fazenda Etec Cidade do Livro</Text>
+        <Text>
+          Uma cesta com produtos selecionados cuidadosamente da fazenda direto
+          para sua cozinha
+        </Text>
+        <Text>R$ 40,00</Text>
+      </View>
+    </>
+  );
+}
+
+const estilos = StyleSheet.create({
+  topo: {
+    width: "100%",
+    height: (578 / 768) * width,
+  },
+
+  titulo: {
+    fontSize: 24,
+    fontWeight: "bold",
+    lineHeight: 26,
+    width: "100%",
+    textAlign: "center",
+    color: "white",
+    position: "absolute",
+    padding: 16,
+  },
+});
